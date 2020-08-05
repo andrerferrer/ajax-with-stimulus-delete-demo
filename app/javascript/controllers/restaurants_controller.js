b57665b89de7d480319ee663446f4da1e3209ede
Andre Ferrer <andrerferrer@hotmail.com>
2020-08-05 18:24:34 -0300

    Finish implementing stimullus controller

--- app/javascript/controllers/restaurants_controller.js
+++ app/javascript/controllers/restaurants_controller.js
@@ -1,7 +1,26 @@
 import { Controller } from "stimulus";
+import Rails from "@rails/ujs";
 
 export default class extends Controller {
   connect() {
-    console.log("Hello world from stimulus's restaurants controller");
+    console.log('Hello from rest controller!');
+  }
+  
+  delete() {
+    const restaurantID = this.element.id;
+    Rails.ajax(
+      {
+        type: "delete",
+        url: `/restaurants/${restaurantID}`,
+        // url: this.data.get('url'),
+        success: function(data) {
+          // If you have any data, you can console log
+          // console.log(data)
+
+          // Remove the restaurant from the DOM
+          document.getElementById(`restaurant-${restaurantID}`).remove();
+        }
+      }
+    )
   }
 }
